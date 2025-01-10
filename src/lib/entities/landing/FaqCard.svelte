fda5db60ce4b12632d7f8d3222853f0781cb47ef
Bear-software <linner_952@mail.ru>
2025-01-10 07:20:56 +0100

    refactoring component

---
+++ src/lib/entities/landing/FaqCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	// import {  } from '$shared';
+	// import {  } from '$widgets';
+	// import {  } from '$entities';
+	import { fade } from 'svelte/transition';
+
+	let showAnswer = $state(false);
+
+	const { a, q } = $props();
+
+
+	console.log(a)
+</script>
+
+
+<style lang="postcss">
+</style>
