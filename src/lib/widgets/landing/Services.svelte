b06966bf6973e65892c7b997776f2c43fb1e6756
Bear-software <linner_952@mail.ru>
2025-01-12 20:30:39 +0100

    refactoring

--- src/lib/widgets/landing/Services.svelte
+++ src/lib/widgets/landing/Services.svelte
@@ -11,42 +11,6 @@
 
 <a name="usługi"></a>
 
-<section class="f-drone wf-section">
-	<img
-		src="images/638dadf7b305ca5a08eed132_drone7.png"
-		loading="eager"
-		srcset="images/638dadf7b305ca5a08eed132_drone7-p-500.png 500w, images/638dadf7b305ca5a08eed132_drone7-p-800.png 800w, images/638dadf7b305ca5a08eed132_drone7-p-1080.png 1080w, images/638dadf7b305ca5a08eed132_drone7.png 1786w"
-		sizes="(max-width: 479px) 100vw, (max-width: 767px) 335px, (max-width: 991px) 100vw, 893px"
-		alt=""
-		class="f-drone--img"
-	/>
-	<div class="container-small">
-		<div class="f-drone--content">
-			<div class="text-wr-wr max-w-722">
-				<div class="text-wr--h2">
-					<h2 class="h2">Latamy, by digitalizować.</h2>
-				</div>
-			</div>
-			<div class="subhead-3 mt-8">
-				Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym.
-			</div>
-			<a href="/" class="link-arr w-inline-block">
-				<div class="inline">Lorem Ipsum jest tekstem stosowanym.</div>
-				<div class="link-arr--ico w-embed">
-					<svg viewBox="0 0 15 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path
-							d="M8 3L8 10C8 14 6 16 2 16H0M8 3L14 7M8 3L2 7"
-							stroke="currentcolor"
-							stroke-width="2"
-							stroke-linejoin="round"
-						></path>
-					</svg>
-				</div>
-			</a>
-		</div>
-	</div>
-</section>
-
 <section class="sec-96 wf-section">
 	<div class="container-small">
 		<div class="f-card--inner">
