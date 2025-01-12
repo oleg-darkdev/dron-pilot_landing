860c461313e1a3457325ec24be47fa79391d29fd
Bear-software <linner_952@mail.ru>
2025-01-12 20:08:44 +0100

    alfa v.

---
+++ src/lib/widgets/landing/FreeBlockSecond.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	// import {  } from '$shared';
+	// import {  } from '$widgets';
+	// import {  } from '$entities'
+</script>
+
+<div class="a-banner">
+	<div class="container-small mod-banner">
+		<div class="a-banner--inner">
+			<div class="a-banner--imgcol"><div>Product light paper</div></div>
+			<div class="a-banner--content">
+				<div>
+					Check out the light paper to learn more about the technology that serves as an engine to
+					our platform, $SPEXI tokens, our roadmap, and more.
+				</div>
+				<a href="#" class="link-arr w-inline-block mt-24"
+					><div class="inline">Read the light paper</div>
+					<div class="link-arr--ico w-embed">
+						<svg viewBox="0 0 15 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path
+								d="M8 3L8 10C8 14 6 16 2 16H0M8 3L14 7M8 3L2 7"
+								stroke="currentcolor"
+								stroke-width="2"
+								stroke-linejoin="round"
+							></path>
+						</svg>
+					</div></a
+				>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+</style>
