e39e7b1befa02ed3aafce94662596ccdc66336b5
Bear-software <linner_952@mail.ru>
2025-01-13 17:39:05 +0100

    upd content

--- src/lib/widgets/landing/About.svelte
+++ src/lib/widgets/landing/About.svelte
@@ -4,7 +4,7 @@
 	// import {  } from '$entities'
 </script>
 
-<a name="onas"></a>
+<a name="o-nas"></a>
 
 <section class="f-drone wf-section">
 	<img
@@ -42,7 +42,7 @@
 	</div>
 </section>
 
-<div class="f-reward wf-section">
+<section class="f-reward wf-section">
 	<div class="container-small">
 		<div class="f-reward--inner">
 			<div class="text-wr-wr">
@@ -53,25 +53,36 @@
 			<div class="f-reward--grid">
 				<div>
 					<div class="f-reward--lext">
-						Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle
-						poligraficznym.
+						Sztuczna inteligencja to fundament naszej działalności. W dronpilot.eu wykorzystujemy
+						zaawansowane algorytmy AI do analizowania danych z dronów, optymalizacji procesów i
+						automatyzacji operacji. Dzięki technologii uczenia maszynowego usprawniamy podejmowanie
+						decyzji, zwiększając efektywność i bezpieczeństwo naszych rozwiązań. AI wspiera nas w
+						precyzyjnym mapowaniu, monitoringu i analizie danych, dostarczając Klientom niezrównaną
+						wartość.
 					</div>
 					<div class="f-reward--lext">
-						Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle
-						poligraficznym.
+						Jesteśmy pionierami w wykorzystaniu nowoczesnych technologii, takich jak IoT,
+						rozszerzona rzeczywistość (AR), oraz zaawansowane rozwiązania do digitalizacji.
+						Integrujemy je z istniejącymi u Klienta systemami IT, czujnikami, czy dronami, aby
+						dostarczać kompleksowe rozwiązania i systemy monitoringu, inspekcji oraz raportowania w
+						czasie rzeczywistym. Nasze innowacje umożliwiają Klientom efektywniejsze zarządzanie
+						procesami w sektorach takich jak: energetyka, przemysł, logistyka budownictwo czy
+						rolnictwo.
 					</div>
-					<div class="f-reward--lext-mob">
-						Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle
-						poligraficznym.
-					</div>
+					<!-- <div class="f-reward--lext-mob">
+						
+					</div> -->
 				</div>
 				<div>
 					<div>
-						Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle
-						poligraficznym.
+						Współpracujemy z liderami w branży technologiach: AI, chmurowych, innowacyjnych
+						interfejsów, takimi jak AWS, Microsoft, Google i Oracle. Nasze partnerstwa umożliwiają
+						nam dostarczanie skalowalnych, bezpiecznych i wydajnych rozwiązań, które spełniają
+						potrzeby nawet najbardziej wymagających Klientów. Wspólnie rozwijamy ekosystem
+						technologiczny, który przyspiesza cyfrową transformację.
 					</div>
-					<a href="/" class="link-arr w-inline-block mt-56">
-						<div class="inline">Lorem Ipsum jest tekstem</div>
+					<a href="#technologie" class="link-arr w-inline-block mt-56">
+						<div class="inline">Nasze możliwości</div>
 						<div class="link-arr--ico w-embed">
 							<svg viewBox="0 0 15 19" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path
@@ -87,7 +98,7 @@
 			</div>
 		</div>
 	</div>
-</div>
+</section>
 
 <style lang="postcss">
 </style>
