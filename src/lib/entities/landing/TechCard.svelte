d662bfd10f21339d56050e4c293785c218edbb71
Bear-software <linner_952@mail.ru>
2025-01-13 22:35:34 +0100

    refactoring

--- src/lib/entities/landing/TechCard.svelte
+++ src/lib/entities/landing/TechCard.svelte
@@ -3,11 +3,11 @@
 	// import {  } from '$widgets';
 	// import {  } from '$entities';
 
-	let { title, icon, shortDesc, anchor, showIdTechFunc } = $props();
+	let { title, icon, shortDesc, anchor, showIdFunc, showIoTFunc, subMenu } = $props();
 </script>
 
-<div class="h-seq--item">
-	<img src={icon} qloading="eager" alt="" class="h-seq--ico" />
+<div class="h-seq--item {subMenu ? 'orange-bg': 'gray-bg'}">
+	<img src={icon} loading="eager" alt="" class="h-seq--ico" />
 	<div class="subhead-3">
 		{title}
 	</div>
@@ -16,7 +16,22 @@
 	</div>
 
 	{#if anchor === 'id'}
-		<button onclick={showIdTechFunc} target="_blank" class="link-tech w-inline-block">
+		<button onclick={showIdFunc} class="link-tech w-inline-block">
+			<div class="inline">{title}</div>
+			<div class="link-tech--ico w-embed">
+				<svg viewBox="0 0 15 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path
+						d="M8 3L8 10C8 14 6 16 2 16H0M8 3L14 7M8 3L2 7"
+						stroke="currentcolor"
+						stroke-width="2"
+						stroke-linejoin="round"
+					></path>
+				</svg>
+			</div>
+		</button>
+	{/if}
+	{#if anchor === 'iot'}
+		<button onclick={showIoTFunc} class="link-tech w-inline-block">
 			<div class="inline">{title}</div>
 			<div class="link-tech--ico w-embed">
 				<svg viewBox="0 0 15 19" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -33,6 +48,44 @@
 </div>
 
 <style lang="postcss">
+	.gray-bg {
+		background-color: rgba(26, 26, 26, 0.1);
+	}
+	.orange-bg {
+		background-color: rgba(242, 143, 83, 0.1);
+	}
+	/* # */
+	.h-seq--item {
+		display: -webkit-box;
+		display: -webkit-flex;
+		display: -ms-flexbox;
+		display: flex;
+		padding: 32px;
+		-webkit-box-orient: vertical;
+		-webkit-box-direction: normal;
+		-webkit-flex-direction: column;
+		-ms-flex-direction: column;
+		flex-direction: column;
+		-webkit-box-align: start;
+		-webkit-align-items: flex-start;
+		-ms-flex-align: start;
+		align-items: flex-start;
+		grid-row-gap: 16px;
+		border-radius: 2px;
+
+		background-image: -webkit-gradient(
+			linear,
+			left top,
+			left bottom,
+			from(transparent),
+			to(rgba(0, 0, 0, 0.1))
+		);
+		background-image: linear-gradient(180deg, transparent, rgba(0, 0, 0, 0.1));
+		-webkit-backdrop-filter: blur(34px);
+		backdrop-filter: blur(34px);
+		color: #fff;
+	}
+
 	.link-tech {
 		color: #fff;
 		font-size: 17px;
@@ -55,11 +108,25 @@
 	.link-tech:hover {
 		color: rgba(0, 0, 0, 0.5);
 	}
+	@media screen and (max-width: 991px) {
+		.h-seq--item {
+			width: 308px;
+			-webkit-box-flex: 0;
+			-webkit-flex: 0 0 auto;
+			-ms-flex: 0 0 auto;
+			flex: 0 0 auto;
+		}
+	}
 
 	@media screen and (max-width: 767px) {
 		.link-tech--ico {
 			width: 11px;
 			height: 16px;
+		}
+
+		.h-seq--item {
+			width: 100%;
+			padding: 24px 24px 24px 20px;
 		}
 	}
 </style>
--- src/lib/widgets/landing/Technologies.svelte
+++ src/lib/widgets/landing/Technologies.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	import { tech, idTech } from '$sharedData';
+	import { tech, idTech, idIoT } from '$sharedData';
 	// import {  } from '$widgets';
 	import { TechCard } from '$entitiesLanding';
 	import { fade } from 'svelte/transition';
 
-	let showIdTech = $state(false);
+	let showIdTech = $state(false),
+		subMenu = $state(false),
+		showIdIoT = $state(false);
 </script>
 
 <a name="technologie"></a>
@@ -25,39 +27,43 @@
 					{icon}
 					{shortDesc}
 					{anchor}
-					showIdTechFunc={() => {
+					showIdFunc={() => {
 						showIdTech = !showIdTech;
-						console.log(showIdTech);
+						showIdIoT = false;
+					}}
+					showIoTFunc={() => {
+						showIdIoT = !showIdIoT;
+						showIdTech = false;
 					}}
 				/>
 			{/each}
 		</div>
-		{#if showIdTech}
+		{#if showIdTech || showIdIoT}
 			<div transition:fade class="container mt-40">
 				<div class="h-seq-title">
 					<div class="text-wr-wr">
 						<div class="text-wr--h2">
-							<h2 class="h2 mod--textgrad">Technologie identyfikacji bezdotykowej.</h2>
+							<h2 class="h2 mod--textgrad">{showIdIoT ? 'Technologie IoT': 'Technologie identyfikacji bezdotykowej.'}</h2>
 						</div>
 					</div>
 				</div>
 				<div class="h-seq--inner">
-					{#each idTech as { title, icon, shortDesc, anchor }}
-						<TechCard {title} {icon} {shortDesc} {anchor} />
+					{#each showIdTech ? idTech : idIoT as { title, icon, shortDesc, anchor }}
+						<TechCard {title} {icon} {shortDesc} {anchor}  subMenu={true}/>
 					{/each}
 				</div>
 			</div>
 		{/if}
 	</div>
 
-	<img
+	<!-- <img
 		src="images/638dadf7b305ca170beed130_block-easy-to-join.jpg"
 		loading="eager"
 		srcset="images/638dadf7b305ca170beed130_block-easy-to-join-p-500.jpg 500w, images/638dadf7b305ca170beed130_block-easy-to-join-p-800.jpg 800w, images/638dadf7b305ca170beed130_block-easy-to-join-p-1080.jpg 1080w, images/638dadf7b305ca170beed130_block-easy-to-join-p-1600.jpg 1600w, images/638dadf7b305ca170beed130_block-easy-to-join-p-2000.jpg 2000w, images/638dadf7b305ca170beed130_block-easy-to-join.jpg 2800w"
 		sizes="(max-width: 767px) 100vw, (max-width: 991px) 96vw, 100vw"
 		alt=""
 		class="h-seq--bg"
-	/>
+	/> -->
 
 	<!-- <a name=""></a> -->
 </section>
