de52bc30d3dde116e0192463d32128000513a08d
Bear-software <linner_952@mail.ru>
2025-01-14 18:02:40 +0100

    add map

--- src/lib/widgets/landing/Contact.svelte
+++ src/lib/widgets/landing/Contact.svelte
@@ -6,21 +6,26 @@
 
 <a name="kontakt"></a>
 
-
 <section class="sec-128">
 	<div class="about-contacts--wr">
 		<div class="text-wr-wr">
 			<div class="text-wr--h2"><h2 class="h2">Kontakt z nami</h2></div>
 		</div>
 		<div class="about-contacts--inner">
-			<div class="about-contacts--item">
-				<div class="about-contacts--subtitle">Polska</div>
-				<div class="about-contacts--title">Lorem impsum</div>
+			<div class="flex flex-col">
+				<div class="about-contacts--item">
+					<div class="about-contacts--subtitle">Polska</div>
+					<div class="about-contacts--title">DronPilot</div>
+				</div>
+				
 			</div>
-			<div class="about-contacts--item">
+			<div class="about-contacts--item z-10">
 				<div class="about-contacts--subtitle">Adres biurowy</div>
-				<div class="subhead-1 mod--a-contact">Kaprów 1 / 26, 84-100 Puck </div>
-				<a href="https://www.google.com/maps/place/Kapr%C3%B3w+1%2F26,+84-100+Puck/@54.7162824,18.423977,17z/data=!3m1!4b1!4m6!3m5!1s0x46fdb21a72482369:0x6ef85e349347a7e7!8m2!3d54.7162824!4d18.423977!16s%2Fg%2F11w83s4_1x?entry=ttu&g_ep=EgoyMDI1MDEwOC4wIKXMDSoASAFQAw%3D%3D"  target="_blank" class="link-arr w-inline-block mt-24"
+				<div class="subhead-1 mod--a-contact">Kaprów 1 / 26, 84-100 Puck</div>
+				<a
+					href="https://www.google.com/maps/place/Kapr%C3%B3w+1%2F26,+84-100+Puck/@54.7162824,18.423977,17z/data=!3m1!4b1!4m6!3m5!1s0x46fdb21a72482369:0x6ef85e349347a7e7!8m2!3d54.7162824!4d18.423977!16s%2Fg%2F11w83s4_1x?entry=ttu&g_ep=EgoyMDI1MDEwOC4wIKXMDSoASAFQAw%3D%3D"
+					target="_blank"
+					class="link-arr w-inline-block mt-24"
 					><div class="inline">Adres na Mapach Google</div>
 					<div class="link-arr--ico w-embed">
 						<svg viewBox="0 0 15 19" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -37,14 +42,25 @@
 		</div>
 		<div class="about-contacts--maps">
 			<!-- images/638dadf7b305ca6a9aeed18b_map%20%281%29.jpg -->
-			<img
+			 <div class="max-w-md px-2 relative">
+					<iframe
+						class=""
+						width="400px"
+						height="400px"
+						frameborder="0"
+						scrolling="no"
+						marginheight="0"
+						marginwidth="0"
+						src="https://www.openstreetmap.org/export/embed.html?bbox=18.41988265514374%2C54.71454738360181%2C18.424705266952518%2C54.71721222804193&amp;layer=mapnik"
+					/>
+				</div>
+			<!-- <img
 				src=""
 				loading="lazy"
 				sizes="(max-width: 479px) 100vw, (max-width: 767px) 95vw, (max-width: 991px) 324px, 544px"
-				
 				alt=""
 				class="about-contacts--map-img"
-			/>
+			/> -->
 			<div class="about-contacts--map-content">
 				<div class="about-contacts--item">
 					<div class="about-contacts--subtitle">Zapytania ogólne</div>
@@ -52,72 +68,12 @@
 				</div>
 				<div class="about-contacts--item">
 					<div class="about-contacts--subtitle">Dział handlowy</div>
-					<a href="mailto:sales@dronpilot.eu" class="about-contacts--link">sales@dronpilot.eu</a
-					>
+					<a href="mailto:pawel@dronpilot.eu" class="about-contacts--link">pawel@dronpilot.eu</a>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
-
-<section class="h-drone mob--top-0 wf-section">
-	<div class="h-drone--inner">
-		<div id="w-node-a87e6253-1433-46f4-6209-f181bc70aa67-68eed0d6" class="h-drone--head">
-			<div class="container">
-				<div class="h-drone--content">
-					<div class="text-wr-wr">
-						<div class="text-wr--h1-drone">
-							<h2 class="h1--drone mod--textgrad">Lataj z nami</h2>
-						</div>
-					</div>
-					<div class="subhead-3">
-						<!-- Become a part of the team pioneering in new industry -->
-						I przejdź na cyfryzację
-					</div>
-				</div>
-			</div>
-		</div>
-		<a href="/" class="gradient-btn w-inline-block">
-			<div class="gradient-btn--text">Zacznij teraz</div>
-			<img
-				src="images/638dadf7b305ca347deed12d_Gradient%20button.jpg"
-				loading="lazy"
-				sizes="(max-width: 479px) 96vw, (max-width: 767px) 95vw, (max-width: 991px) 96vw, 37vw"
-				srcset="images/638dadf7b305ca347deed12d_Gradient%2520button-p-500.jpg 500w, images/638dadf7b305ca347deed12d_Gradient%2520button-p-800.jpg 800w, images/638dadf7b305ca347deed12d_Gradient%2520button-p-1080.jpg 1080w, images/638dadf7b305ca347deed12d_Gradient%2520button-p-1600.jpg 1600w, images/638dadf7b305ca347deed12d_Gradient%20button.jpg 1748w"
-				alt=""
-				class="gradient-btn--bg"
-			/>
-		</a>
-		<a href="/" class="btn-ico w-inline-block">
-			<div class="btn-ico-wr">
-				<div class="w-embed">
-					<svg
-						width="32"
-						height="24"
-						viewBox="0 0 32 24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							d="M4.87109 6.24242C4.87109 5.73736 5.04002 5.3207 5.37785 4.99242C5.71568 4.66414 6.15488 4.5 6.69542 4.5C7.22631 4.5 7.65584 4.66161 7.98403 4.98485C8.32186 5.31818 8.49078 5.75252 8.49078 6.28788C8.49078 6.77273 8.3267 7.17676 7.99851 7.5C7.66067 7.83333 7.21665 8 6.66646 8H6.65198C6.12109 8 5.69156 7.83333 5.36337 7.5C5.03518 7.16667 4.87109 6.74747 4.87109 6.24242ZM5.05932 19.5V9.37879H8.2736V19.5H5.05932ZM10.0545 19.5H13.2688V13.8485C13.2688 13.4949 13.3074 13.2222 13.3846 13.0303C13.5197 12.6869 13.7249 12.3965 14 12.1591C14.2751 11.9217 14.6201 11.803 15.0352 11.803C16.1163 11.803 16.6568 12.5657 16.6568 14.0909V19.5H19.8711V13.697C19.8711 12.202 19.5333 11.0682 18.8576 10.2955C18.1819 9.52273 17.2891 9.13636 16.179 9.13636C14.9338 9.13636 13.9638 9.69697 13.2688 10.8182V10.8485H13.2543L13.2688 10.8182V9.37879H10.0545C10.0738 9.70202 10.0834 10.7071 10.0834 12.3939C10.0834 14.0808 10.0738 16.4495 10.0545 19.5Z"
-							fill="currentcolor"
-						></path>
-					</svg>
-				</div>
-			</div>
-			<div class="text-block">Linkedin społeczność</div>
-		</a>
-	</div>
-	<img
-		src="images/638dadf7b305ca3264eed12c_drone5.png"
-		loading="lazy"
-		srcset="images/638dadf7b305ca3264eed12c_drone5-p-500.png 500w, images/638dadf7b305ca3264eed12c_drone5-p-800.png 800w, images/638dadf7b305ca3264eed12c_drone5.png 1016w"
-		sizes="(max-width: 479px) 83vw, (max-width: 767px) 310px, 100vw"
-		alt=""
-		class="h-drone--img"
-	/>
-</section>
-
 
 <style lang="postcss">
 	.gradient-btn:hover .gradient-btn--bg {
