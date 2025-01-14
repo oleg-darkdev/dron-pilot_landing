e8039e8c2ddefc372e7c89af468f36e0bed4a60c
Bear-software <linner_952@mail.ru>
2025-01-14 18:03:17 +0100

    clear members cards

--- src/lib/widgets/landing/Team.svelte
+++ src/lib/widgets/landing/Team.svelte
@@ -132,30 +132,6 @@
 	</div>
 </section>
 
-<!-- <div class="sec-128-56"> -->
-<section class="container-small">
-	<!-- <h2>Nasz zespół</h2> -->
-	<!-- mt-56 -->
-	<div class="w-dyn-list">
-		<div role="list" class="c-culture--teams w-dyn-items">
-			{#each team as member}
-				<div role="listitem" class="w-dyn-item">
-					<img
-						alt={member.bio}
-						loading="eager"
-						src={member.img}
-						sizes="(max-width: 479px) 44vw, (max-width: 767px) 46vw, (max-width: 991px) 42vw, 299.34375px"
-						class="c-culture--teams-img"
-					/>
-					<div class="c-culture--teams-bootom">
-						<div>{member.bio}</div>
-						<div class="a-team--slide-position">{member.role}</div>
-					</div>
-				</div>
-			{/each}
-		</div>
-	</div>
-</section>
 
 <!-- </div> -->
 
